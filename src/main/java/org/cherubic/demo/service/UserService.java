0e07fe7a6d175ada0d87d4dca911a8965d82d684
cutiechi <chi@cutie.ooo>
2018-09-03 19:27:02 +0800

    feat(service): add list all user method

--- src/main/java/org/cherubic/demo/service/UserService.java
+++ src/main/java/org/cherubic/demo/service/UserService.java
@@ -1,5 +1,14 @@
 package org.cherubic.demo.service;
 
-public interface UserService {
+import org.cherubic.demo.model.dto.ServiceResult;
+import org.cherubic.demo.model.entity.User;
 
+import java.util.List;
+
+public interface UserService {
+    
+    /**
+     * 获取全部用户列表
+     */
+    ServiceResult<List<User>> listAll ();
 }
--- src/main/java/org/cherubic/demo/service/impl/UserServiceImpl.java
+++ src/main/java/org/cherubic/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,28 @@
 package org.cherubic.demo.service.impl;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import org.cherubic.demo.dao.UserDao;
+import org.cherubic.demo.model.dto.ServiceResult;
+import org.cherubic.demo.model.entity.User;
 import org.cherubic.demo.service.UserService;
+import org.cherubic.demo.util.MybatisUtils;
+
+import java.util.List;
 
 public class UserServiceImpl implements UserService {
-
+    
+    private final UserDao userDao;
+    
+    public UserServiceImpl () {
+        SqlSessionFactory sqlSessionFactory = MybatisUtils.getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        userDao = sqlSession.getMapper(UserDao.class);
+    }
+    
+    @Override
+    public ServiceResult<List<User>> listAll () {
+        return new ServiceResult<>(true, "获取全部用户列表成功", userDao.listAll());
+    }
 }
