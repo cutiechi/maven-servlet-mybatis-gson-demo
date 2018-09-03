fde44953fe02851e410d10a4e96a4e0f310df7dd
cutiechi <chi@cutie.ooo>
2018-09-03 18:49:52 +0800

    feat(dao): add list all user interface

--- src/main/java/org/cherubic/demo/dao/UserDao.java
+++ src/main/java/org/cherubic/demo/dao/UserDao.java
@@ -1,5 +1,15 @@
 package org.cherubic.demo.dao;
 
-public interface UserDao {
+import org.cherubic.demo.model.entity.User;
 
+import java.util.List;
+
+public interface UserDao {
+    
+    /**
+     * 获取全部用户列表
+     *
+     * @return 全部用户列表
+     */
+    List<User> listAll ();
 }
